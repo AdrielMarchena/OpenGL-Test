554d8d5f8a82d1af0e1980863ee7684d53ee51b8
AdrielMarchena <Adriel_contato@hotmail.com>
2020-11-10 16:27:28 -0300

    Changed to pure virtual class

--- src/tests/Test.h
+++ src/tests/Test.h
@@ -6,11 +6,11 @@ namespace test {
 	{
 	public:
 		Test(){}
-		virtual ~Test() {}
+		virtual ~Test() {};
 
-		virtual void OnUpdate(float deltaTime) {}
-		virtual void OnRender() {}
-		virtual void OnImGuiRender() {}
+		virtual void OnUpdate(float deltaTime) = 0;
+		virtual void OnRender() = 0;
+		virtual void OnImGuiRender() = 0;
 	};
 
 }
