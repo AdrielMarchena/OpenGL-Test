359c13e9b39e25de6491cdda46294e93edccad7a
AdrielMarchena <Adriel_contato@hotmail.com>
2020-11-10 16:23:40 -0300

    Added a class that creates a Menu for the tests classes

---
+++ src/tests/Tester.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Test.h"
+namespace test {
+
+	class Tester : public Test
+	{
+	private:
+		Test* m_ActualTest;
+
+		bool m_CubeSelected = false;
+		bool m_TriangleSelected = false;
+		bool m_ClearColorSelected = false;
+	public:
+		Tester();
+		~Tester();
+
+		void OnUpdate(float deltaTime) override;
+		void OnRender() override;
+		void OnImGuiRender() override;
+	private:
+		inline void showMenu();
+	};
+
+}
